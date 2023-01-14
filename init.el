ff6467659c43b883647e67d6825d39542bab332d
Babin Ion <ion.babin@ebs-integrator.com>
2023-01-14 16:12:09 +0200

    add magit

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
-;; Don't use custom
+;; Don't use custom file
 (setq custom-file null-device)
+
+;: GUI Tweaks
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -8,9 +10,9 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 150)
-(load-theme 'modus-vivendi :no-confirm)
 (global-set-key (kbd "<f5>") 'modus-themes-toggle)
 
+;; Enable line numbers
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
@@ -23,8 +25,8 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-unset-key (kbd "C-z"))
 
+;; Package configuration
 (require 'package)
-
 (setq package-archives '(("mepla". "https://melpa.org/packages/")
 			 ("org". "https://orgmode.org/elpa/")
 			 ("elpa". "https://elpa.gnu.org/packages/")))
@@ -41,7 +43,13 @@
 ;; Ensures all packages are installed before running
 (setq use-package-always-ensure t)
 
+;; Theme
 (use-package modus-themes)
+(load-theme 'modus-vivendi :no-confirm)
+;; Fonts
+(use-package all-the-icons)
+
+;; Autocompletion
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
@@ -56,21 +64,27 @@
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode)
-(use-package all-the-icons)
-(add-to-list 'load-path "~/org-mode/lisp")
 
+;; Org-specific configuration
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
 
 (setq org-directory "~/org")
 (setq org-default-notes-file (concat org-directory "/me/time.org"))
-;; (setq org-agenda-files '((concat org-directory "/me/time.org")))
-(setq org-agenda-files '("~/org/work/work.org" "~/org/me/time.org"))
-
+(setq org-agenda-files '(org-default-notes-file))
+(setq org-agenda-block-separator 8411)
 (setq org-capture-templates
        '(("t" "todo" entry (file org-default-notes-file)
 	  "* TODO %? \nSCHEDULED: %^t" :clock-in t :clock-resume t)
 	 ("i" "Idea" entry (file org-default-notes-file)
 	  "* %? :IDEA: \n%t" :clock-in t :clock-resume t)
 	 ("n" "Next Task" entry (file+headline org-default-notes-file "Tasks")
-	  "** NEXT %? \nDEADLINE: %t") ))
+	  "* NEXT %? \nDEADLINE: %t") ))
+
+(use-package org-superstar)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+
+;; Magit config
+(use-package magit
+  :ensure t
+  )
