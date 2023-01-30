6800baf6ff3c15fa2c859930b28ddcaa47dabfc5
Babin Ion <ion.babin@ebs-integrator.com>
2023-01-30 21:49:14 +0200

    customize org-agenda

--- init.el
+++ init.el
@@ -74,17 +74,59 @@
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
 
-(with-eval-after-load 'org
-  (setq org-directory "~/org")
-  (setq org-default-notes-file "~/org/time.org")
-  (setq org-agenda-files '("~/org/time.org"))
-  (setq org-agenda-block-separator 8411)
-  (setq org-todo-keywords '("TODO" "NEXT" "|" "CANCELLED" "DONE"))
-  (setq org-capture-templates
-	'(("t" "todo" entry (file org-default-notes-file)
-	   "* TODO %? \n%^t")
-	  ("n" "Next Task" entry (file+headline org-default-notes-file "Tasks")
-	   "** NEXT %? \nDEADLINE: %t") )))
+(setq org-directory "~/org")
+(setq org-default-notes-file "~/org/time.org")
+(setq org-agenda-files '("~/org/time.org"))
+(setq org-agenda-block-separator 8411)
+(setq org-columns-default-format "%50ITEM(Task) %10CLOCKSUM %16TIMESTAMP_IA")
+(setq org-todo-keywords '("TODO" "NEXT" "|" "CANCELLED" "DONE"))
+(setq org-todo-keyword-faces
+      '(("CANCELLED" . "gray")))
+(setq org-capture-templates
+      '(("t" "todo" entry (file org-default-notes-file)
+	 "* TODO %? \nSCHEDULED: %^t")
+	("n" "Next Task" entry (file+headline org-default-notes-file "Tasks")
+	   "** NEXT %? \nDEADLINE: %t")))
+
+(defvar prot-org-custom-daily-agenda
+    '((agenda "" ((org-agenda-span 1)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-scheduled-past-days 0)
+                ;; We don't need the `org-agenda-date-today'
+                ;; highlight because that only has a practical
+                ;; utility in multi-day views.
+                (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+                (org-agenda-format-date "%A %-e %B %Y")
+                (org-agenda-overriding-header "Today's agenda\n")))
+    (agenda "" ((org-agenda-start-on-weekday nil)
+                (org-agenda-start-day "+1d")
+                (org-agenda-span 3)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                (org-agenda-overriding-header "\nNext three days\n")))
+    (agenda "" ((org-agenda-start-on-weekday nil)
+                ;; We don't want to replicate the previous section's
+                ;; three days, so we start counting from the day after.
+                (org-agenda-start-day "+4d")
+                (org-agenda-span 14)
+                (org-agenda-show-all-dates nil)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-agenda-entry-types '(:deadline))
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                (org-agenda-overriding-header "\nUpcoming deadlines (+14d)\n"))))
+  "Custom agenda for use in `org-agenda-custom-commands'.")
+
+(setq org-agenda-custom-commands
+      `(("A" "Daily agenda and top priority tasks"
+         ,prot-org-custom-daily-agenda)
+	("W" "Work-related tasks" tags-todo "work")
+	("C" "Computer-science-related tasks" tags-todo "cs")
+	("S" "School-related tasks" tags-todo "school")))
+     
+
 (use-package ox-hugo)
 (use-package projectile
   :config
